3d78014c126721839dfa56ca2450ae0a5eae6cd4
lemuriantimewars <102565167+lemuriantimewars@users.noreply.github.com>
2022-03-31 16:37:55 +0900

    Update milkomeda_pools.js

--- src/features/configure/vault/milkomeda_pools.js
+++ src/features/configure/vault/milkomeda_pools.js
@@ -1,1 +1,29 @@
-[{}]
+export const milkomedaPools = [
+    {
+    id: 'milkyswap-wada-multiweth',
+    name: 'Milkyswap WADA multiWETH',
+    token: 'WADA-multiWETH milkyLP',
+    tokenDescription: 'Milkyswap',
+    tokenAddress: '0x11f34aa0648b8bCB805Ae69Bf0063bB900c99Ab7',
+    tokenDecimals: 18,
+    tokenDescriptionUrl: '#',
+    earnedToken: 'lVAULT-WADA-multiWETH-Milky',
+    earnedTokenAddress: '0xDde301921277E2588aD5E5C7Ea0b7dC5808ff87A',
+    earnContractAddress: '0xDde301921277E2588aD5E5C7Ea0b7dC5808ff87A',
+    pricePerFullShare: 1,
+    tvl: 0,
+    oracle: 'lps',
+    oracleId: 'milkyswap-wada-multiweth',
+    oraclePrice: 0,
+    depositsPaused: false,
+    status: 'active',
+    platform: 'Milkyswap',
+    assets: ['WADA', 'multiWETH'],
+    risks: ['COMPLEXITY_LOW', 'NEW_STRAT', 'IL_LOW', 'MCAP_LARGE', 'AUDIT', 'CONTRACTS_VERIFIED'],
+    stratType: 'StratMultiLP',
+    withdrawalFee: '0%',
+    addLiquidityUrl:
+      'https://app.stellaswap.com/exchange/add/0x922D641a426DcFFaeF11680e5358F34d97d112E1/0xAcc15dC74880C9944775448304B263D191c6077F',
+  },
+  ];
+  
